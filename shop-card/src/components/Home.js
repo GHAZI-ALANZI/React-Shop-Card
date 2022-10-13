33f9c209494e8ed304d3154950207b7045829e80
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2022-10-13 10:40:14 +0100

    add img and home page is done

---
+++ shop-card/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../src/App.css'
+
+
+
+const Home = () => {
+  return <div >
+    <h1 className="title text-center ">Welcome to our Shop</h1>
+</div>;
+};
+
+export default Home;
